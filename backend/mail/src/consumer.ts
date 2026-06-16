5ce0b8f03a7b71a0144f52e3eda1df4d0c3e2862
malaytrivedi555 <malaytrivedi555@gmail.com>
2026-06-16 10:46:06 +0530

    use gmail service transport

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -36,9 +36,7 @@ export const startSendOtpConsumer = async () => {
         }
 
         const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.USER,
     pass: process.env.PASSWORD,
