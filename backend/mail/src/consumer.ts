66605a5db939a3d03c183956c63da3e168c27c24
malaytrivedi555 <malaytrivedi555@gmail.com>
2026-06-16 11:33:52 +0530

    Changed to HTTP API

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -1,4 +1,5 @@
 import amqplib from 'amqplib';
+import axios from "axios";
 
 import nodemailer from "nodemailer";
 import dns from "dns";
@@ -39,25 +40,37 @@ export const startSendOtpConsumer = async () => {
           return;
         }
 
-        const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_LOGIN,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+        
 
 
 console.log("About to send mail to:", to);
 
-await transporter.sendMail({
-  from: "malaytrivedi555@gmail.com",
-  to,
-  subject: "Your OTP",
-  text: `Your OTP is ${otp}. It is valid for 5 minutes`,
-});
+
+await axios.post(
+  "https://api.brevo.com/v3/smtp/email",
+  {
+    sender: {
+      name: "ChatSphere",
+      email: "malaytrivedi555@gmail.com",
+    },
+    to: [
+      {
+        email: to,
+      },
+    ],
+    subject: "Your OTP",
+    textContent: `Your OTP is ${otp}. It is valid for 5 minutes.`,
+  },
+  {
+    headers: {
+      "api-key": process.env.BREVO_API_KEY!,
+      "Content-Type": "application/json",
+    },
+  }
+);
+
+console.log(`OTP mail sent to ${to}`);
+channel.ack(msg);
 
 console.log(`OTP mail sent to ${to}`);
 
